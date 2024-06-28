9e23ffb579d328a85495391e1f8aa31ce5cf1e62
Kachura Darya <kachuradarya4426@gmail.com>
2024-06-28 14:37:23 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.nupn = new System.Windows.Forms.NumericUpDown();
+            this.nupm = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.RosyBrown;
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown1);
+            this.splitContainer1.Panel1.Controls.Add(this.nupn);
+            this.splitContainer1.Panel1.Controls.Add(this.nupm);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 566);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(61, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Количество больных";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 130);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 26);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // nupn
+            // 
+            this.nupn.Location = new System.Drawing.Point(3, 80);
+            this.nupn.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nupn.Name = "nupn";
+            this.nupn.Size = new System.Drawing.Size(55, 26);
+            this.nupn.TabIndex = 5;
+            this.nupn.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nupn.ValueChanged += new System.EventHandler(this.nupn_ValueChanged);
+            // 
+            // nupm
+            // 
+            this.nupm.Location = new System.Drawing.Point(3, 32);
+            this.nupm.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nupm.Name = "nupm";
+            this.nupm.Size = new System.Drawing.Size(55, 26);
+            this.nupm.TabIndex = 4;
+            this.nupm.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nupm.ValueChanged += new System.EventHandler(this.nupm_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(61, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ширина поля";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(57, 480);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(64, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Длина поля";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(57, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.RosyBrown;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(529, 563);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 566);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown nupn;
+        private System.Windows.Forms.NumericUpDown nupm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private int m;
+        private int n;
+        private int cellSize = 10;
+        private Random rand = new Random();
+        private List<Human> humans = new List<Human>();
+        private List<InfectedHuman> infectedHumans = new List<InfectedHuman>();
+        private List<IncubationHuman> incubationHumans = new List<IncubationHuman>();
+        private List<RecoveredHuman> recoveredHumans = new List<RecoveredHuman>();
+        private List<DeadHuman> deadHumans = new List<DeadHuman>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Инициализация таймера
+            timer2.Interval = 500; // интервал в миллисекундах
+            timer2.Tick += Timer2_Tick;
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nupm_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nupn_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            humans.Clear();
+            infectedHumans.Clear();
+            incubationHumans.Clear();
+            recoveredHumans.Clear();
+            deadHumans.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m = (int)nupm.Value; // размер поля по X
+            n = (int)nupn.Value; // размер поля по Y
+
+            humans.Clear();
+            infectedHumans.Clear();
+            incubationHumans.Clear();
+            recoveredHumans.Clear();
+            deadHumans.Clear();
+
+            // Создаем изображение для отрисовки поля
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                Pen pen = new Pen(Color.Black);
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        g.DrawRectangle(pen, i * cellSize, j * cellSize, cellSize, cellSize);
+                    }
+                }
+            }
+
+            // Определяем количество зараженных людей (красных клеток)
+            int totalCells = m * n;
+            int humanCount = totalCells / 4; // четверть клеток будут зелеными (люди)
+            int infectedCount = (int)numericUpDown1.Value;
+
+            var allPositions = Enumerable.Range(0, totalCells).ToList();
+            var infectedPositions = allPositions.OrderBy(x => rand.Next()).Take(infectedCount).ToList();
+            var humanPositions = allPositions.Except(infectedPositions).Take(humanCount).ToList();
+
+            // Добавляем зараженных людей
+            foreach (var pos in infectedPositions)
+            {
+                int x = pos % m;
+                int y = pos / m;
+                infectedHumans.Add(new InfectedHuman(x, y));
+            }
+
+            // Добавляем обычных людей
+            foreach (var pos in humanPositions)
+            {
+                int x = pos % m;
+                int y = pos / m;
+                humans.Add(new Human(x, y));
+            }
+
+            pictureBox1.Image = bmp;
+
+            // Запускаем таймер для автоматического перемещения клеток
+            timer2.Start();
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            // Перемещение зараженных людей
+            foreach (var infected in infectedHumans.ToList())
+            {
+                infected.Move(m, n, rand, humans, infectedHumans, incubationHumans, recoveredHumans, deadHumans);
+            }
+
+            // Перемещение обычных людей
+            foreach (var human in humans.ToList())
+            {
+                human.Move(m, n, rand, humans, infectedHumans, incubationHumans, recoveredHumans, deadHumans);
+            }
+
+            // Перемещение инкубационных людей
+            foreach (var incubation in incubationHumans.ToList())
+            {
+                incubation.Move(m, n, rand, humans, infectedHumans, incubationHumans, recoveredHumans, deadHumans);
+            }
+
+            // Перемещение выздоровевших людей
+            foreach (var recovered in recoveredHumans.ToList())
+            {
+                recovered.Move(m, n, rand, humans, infectedHumans, incubationHumans, recoveredHumans, deadHumans);
+            }
+
+            // Перемещение мертвых людей
+            foreach (var dead in deadHumans.ToList())
+            {
+                dead.Move(m, n, rand, humans, infectedHumans, incubationHumans, recoveredHumans, deadHumans);
+            }
+
+            // Обновление заражения и инкубации
+            CheckInfection();
+
+            // Обновление отображения на PictureBox
+            UpdatePictureBox();
+        }
+
+       
+
+
+        private void UpdatePictureBox()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                Pen pen = new Pen(Color.Black);
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        g.DrawRectangle(pen, i * cellSize, j * cellSize, cellSize, cellSize);
+                    }
+                }
+
+                // Рисуем мертвых людей (черные клетки)
+                foreach (var dead in deadHumans)
+                {
+                    g.FillRectangle(Brushes.Black, dead.X * cellSize, dead.Y * cellSize, cellSize, cellSize);
+                }
+
+                // Рисуем выздоровевших людей (фиолетовые клетки)
+                foreach (var recovered in recoveredHumans)
+                {
+                    g.FillRectangle(Brushes.Purple, recovered.X * cellSize, recovered.Y * cellSize, cellSize, cellSize);
+                }
+
+                // Рисуем инкубационные клетки (оранжевые клетки)
+                foreach (var incubation in incubationHumans)
+                {
+                    g.FillRectangle(Brushes.Orange, incubation.X * cellSize, incubation.Y * cellSize, cellSize, cellSize);
+                }
+
+                // Рисуем обычных людей (зеленые клетки)
+                foreach (var human in humans)
+                {
+                    g.FillRectangle(Brushes.Green, human.X * cellSize, human.Y * cellSize, cellSize, cellSize);
+                }
+
+                // Рисуем зараженных людей (красные клетки)
+                foreach (var infected in infectedHumans)
+                {
+                    g.FillRectangle(Brushes.Red, infected.X * cellSize, infected.Y * cellSize, cellSize, cellSize);
+                }
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        public class Human
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Human(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public virtual void Move(int m, int n, Random rand, List<Human> humans, List<InfectedHuman> infectedHumans, List<IncubationHuman> incubationHumans, List<RecoveredHuman> recoveredHumans, List<DeadHuman> deadHumans)
+            {
+                // Перемещение на случайную соседнюю клетку
+                int newX = X + rand.Next(-1, 2);
+                int newY = Y + rand.Next(-1, 2);
+
+                // Убедимся, что новые координаты находятся в пределах границ и клетка свободна
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n)
+                {
+                    if (!humans.Any(h => h.X == newX && h.Y == newY) && !infectedHumans.Any(ih => ih.X == newX && ih.Y == newY) && !incubationHumans.Any(i => i.X == newX && i.Y == newY) && !recoveredHumans.Any(r => r.X == newX && r.Y == newY) && !deadHumans.Any(d => d.X == newX && d.Y == newY))
+                    {
+                        X = newX;
+                        Y = newY;
+                    }
+                }
+            }
+        }
+
+        public class InfectedHuman : Human
+        {
+            public InfectedHuman(int x, int y) : base(x, y)
+            {
+            }
+
+            public override void Move(int m, int n, Random rand, List<Human> humans, List<InfectedHuman> infectedHumans, List<IncubationHuman> incubationHumans, List<RecoveredHuman> recoveredHumans, List<DeadHuman> deadHumans)
+            {
+                // Перемещение на случайную соседнюю клетку
+                int newX = X + rand.Next(-1, 2);
+                int newY = Y + rand.Next(-1, 2);
+
+                // Убедимся, что новые координаты находятся в пределах границ и клетка свободна
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n)
+                {
+                    if (!humans.Any(h => h.X == newX && h.Y == newY) && !infectedHumans.Any(ih => ih.X == newX && ih.Y == newY) && !incubationHumans.Any(i => i.X == newX && i.Y == newY) && !recoveredHumans.Any(r => r.X == newX && r.Y == newY) && !deadHumans.Any(d => d.X == newX && d.Y == newY))
+                    {
+                        X = newX;
+                        Y = newY;
+                    }
+                }
+            }
+        }
+
+        public class IncubationHuman : Human
+        {
+            public IncubationHuman(int x, int y) : base(x, y)
+            {
+            }
+
+            public override void Move(int m, int n, Random rand, List<Human> humans, List<InfectedHuman> infectedHumans, List<IncubationHuman> incubationHumans, List<RecoveredHuman> recoveredHumans, List<DeadHuman> deadHumans)
+            {
+                // Перемещение на случайную соседнюю клетку
+                int newX = X + rand.Next(-1, 2);
+                int newY = Y + rand.Next(-1, 2);
+
+                // Убедимся, что новые координаты находятся в пределах границ и клетка свободна
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n)
+                {
+                    if (!humans.Any(h => h.X == newX && h.Y == newY) && !infectedHumans.Any(ih => ih.X == newX && ih.Y == newY) && !incubationHumans.Any(i => i.X == newX && i.Y == newY) && !recoveredHumans.Any(r => r.X == newX && r.Y == newY) && !deadHumans.Any(d => d.X == newX && d.Y == newY))
+                    {
+                        X = newX;
+                        Y = newY;
+                    }
+                }
+            }
+        }
+
+        public class RecoveredHuman : Human
+        {
+            public RecoveredHuman(int x, int y) : base(x, y)
+            {
+            }
+
+            public override void Move(int m, int n, Random rand, List<Human> humans, List<InfectedHuman> infectedHumans, List<IncubationHuman> incubationHumans, List<RecoveredHuman> recoveredHumans, List<DeadHuman> deadHumans)
+            {
+                // Перемещение на случайную соседнюю клетку
+                int newX = X + rand.Next(-1, 2);
+                int newY = Y + rand.Next(-1, 2);
+
+                // Убедимся, что новые координаты находятся в пределах границ и клетка свободна
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n)
+                {
+                    if (!humans.Any(h => h.X == newX && h.Y == newY) && !infectedHumans.Any(ih => ih.X == newX && ih.Y == newY) && !incubationHumans.Any(i => i.X == newX && i.Y == newY) && !recoveredHumans.Any(r => r.X == newX && r.Y == newY) && !deadHumans.Any(d => d.X == newX && d.Y == newY))
+                    {
+                        X = newX;
+                        Y = newY;
+                    }
+                }
+            }
+        }
+
+        public class DeadHuman : Human
+        {
+            public DeadHuman(int x, int y) : base(x, y)
+            {
+            }
+
+            public override void Move(int m, int n, Random rand, List<Human> humans, List<InfectedHuman> infectedHumans, List<IncubationHuman> incubationHumans, List<RecoveredHuman> recoveredHumans, List<DeadHuman> deadHumans)
+            {
+                // Мертвые люди не двигаются
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
